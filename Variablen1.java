45e7295c893db03bd54beffdcc03b4acbd3c88d3
frede <frederick.dathe@outlook.com>
2021-01-24 19:25:35 +0100

    Initial commit

---
+++ Variablen1.java
@@ -0,0 +1,31 @@
+
+public class Variablen1 {
+        public static void main (String [] args){
+        
+        byte bZahl = 28;
+        
+        short sZahl = -18453;
+        
+        int iZahl = 4356576;
+        
+        long lZahl = 345236577970L;
+        
+        float fZahl =  4.37456678f;
+        
+        double dZahl = 3645.564782;
+        
+        boolean bestanden = true;
+        
+        char zeichen = '%' ;
+        
+        System.out.println("bZahl = " + bZahl );
+        System.out.println("sZahl = " + sZahl);
+        System.out.println("iZahl = " + iZahl);
+        System.out.println("lZahl = " + lZahl);
+        System.out.println("fZahl = " + fZahl);
+        System.out.println("dZahl = " + dZahl);
+        System.out.println("bestanden = " + bestanden);
+        System.out.println("zeichen = " + zeichen);
+        
+        }
+}
---
+++ Variablen2.java
@@ -0,0 +1,17 @@
+
+public class Variablen2 {
+        public static void main (String [] args){
+        
+        char z1 = 'a';
+        char z2 = 'b';
+        char z3 = 'A';
+        char z4 = 0x00a9;
+        char z5 = 0x00be;
+        
+        System.out.println("z1: " + z1);
+        System.out.println("z2: " + z2);
+        System.out.println("z3: " + z3);
+        System.out.println("z4: " + z4);
+        System.out.println("z5: " + z5);
+        }
+}
